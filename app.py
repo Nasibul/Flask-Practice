5341ed43f429d736e60d8e39a6cd0707ff63d351
Nasibul <Nasibulh@yahoo.com>
2022-10-05 23:10:51 -0400

    Inputs linked

--- app.py
+++ app.py
@@ -1,22 +1,34 @@
-import numpy as np
-from flask import Flask, request,render_template
+from flask import Flask, request, render_template
 import pickle
+import pandas as pd
 
 app = Flask(__name__)
 model = pickle.load(open('model.pickle', 'rb'))
+encoder = pickle.load(open('encoder.pickle', 'rb'))
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict',methods=['GET', 'POST'])
 def predict():
     '''
     For rendering results on HTML GUI
     '''
-    prediction = model.predict([request.form.values()])
+    inputs=[]
+    inputs.append(str(request.form.get("City")))
+    inputs.append(str(request.form.get("Gender")))
+    inputs.append(int(request.form.get("age")))
+    inputs.append(float(request.form.get("income")))
+    
+    num = inputs[2:]
+    cat = inputs[0:2]
+    cat = encoder.transform([cat])
+    inputs = pd.DataFrame(list(cat.flat) + num).T
 
-    return render_template('index.html', prediction_text='Illness? {}'.format(prediction))
+    prediction = model.predict(inputs)[0]
+    
+    return render_template('index.html', prediction_text='Illness: {}'.format(prediction))
 
 if __name__ == "__main__":
     app.run(debug=True)
